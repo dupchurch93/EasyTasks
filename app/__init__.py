1e5bafa091d9dba98b36e9aa5a157e28e1555c38
Daniel Upchurch <danielupchurch93@gmail.com>
2021-05-03 14:56:28 -0400

    solved

--- app/__init__.py
+++ app/__init__.py
@@ -49,13 +49,13 @@ def inject_csrf_token(response):
     return response
 
 # Connect the Flask app to the React app
-# @app.route('/', defaults={'path': ''})
-# @app.route('/<path:path>')
-# def react_root(path):
-#     print("path", path)
-#     if path == 'favicon.ico':
-#         return app.send_static_file('favicon.ico')
-#     return app.send_static_file('index.html')
-@app.route("/")
-def hello_world():
-    return "hello world"
+@app.route('/', defaults={'path': ''})
+@app.route('/<path:path>')
+def react_root(path):
+    print("path", path)
+    if path == 'favicon.ico':
+        return app.send_static_file('favicon.ico')
+    return app.send_static_file('index.html')
+# @app.route("/")
+# def hello_world():
+#     return "hello world"
