668cb26eaf438215bdea42d296cdc5dfb6e862a3
Antonio <anguilar@aol.com>
2021-05-03 14:21:26 -0700

    establishing users

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,25 +1,35 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import { useDispatch } from "react-redux";
+import { restoreUser } from "./store/session";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
+  const dispatch = useDispatch();
+  const [loaded, setLoaded] = useState(false);
+
+  // Upon loading the App component restoreUser is fired
+  useEffect(() => {
+    dispatch(restoreUser()).then(() => {
+      setLoaded(true);
+    });
+  }, [dispatch]);
+
+  // Nothing is displayed if the user is not restored
+  if (!loaded) {
+    return null;
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      {/* Navbar will be here */}
+      <Switch>
+        <Route path="/" exact={true}>
+          {/* <Home /> */}
+        </Route>
+      </Switch>
+    </BrowserRouter>
   );
 }
 
--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,14 +1,32 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { Provider } from "react-redux";
+import { BrowserRouter } from "react-router-dom";
+import configureStore from "./store";
+
+const store = configureStore();
+
+if (process.env.NODE_ENV !== "production") {
+  window.store = store;
+}
+
+// Initialize a root with routing and the redux store providers
+const Root = () => (
+  <BrowserRouter>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </BrowserRouter>
+);
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Root />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
