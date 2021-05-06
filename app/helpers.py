0fc8e482eb7d0c647733383935584d6af5b5b2fc
Antonio <anguilar@aol.com>
2021-05-06 10:17:44 -0700

    added amazon s3 support

---
+++ app/helpers.py
@@ -0,0 +1,34 @@
+from enum import unique
+import boto3
+import botocore
+from .config import S3_KEY, S3_SECRET, S3_BUCKET, S3_LOCATION
+import uuid
+
+
+s3 = boto3.client(
+    "s3",
+    aws_access_key_id=S3_KEY,
+    aws_secret_access_key=S3_SECRET
+)
+
+
+def upload_file_to_s3(file, bucket_name=S3_BUCKET, acl="public-read"):
+    print(bucket_name, S3_KEY, S3_SECRET)
+    unique_filename = uuid.uuid4().hex + ".png"
+    try:
+        s3.upload_fileobj(
+            file,
+            bucket_name,
+            unique_filename,
+            ExtraArgs={
+                "ACL": acl,
+                "ContentType": file.content_type
+            }
+        )
+
+    except Exception as e:
+        # This is a catch all exception, edit this part to fit your needs.
+        print("Something Happened: ", e)
+        return e
+
+    return f"region=us-east-2{unique_filename}"
