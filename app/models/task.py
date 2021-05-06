db0f0c62d488db4f4f3983760b724e597f45bec4
Antonio <anguilar@aol.com>
2021-05-06 10:37:43 -0700

    added task model

---
+++ app/models/task.py
@@ -0,0 +1,25 @@
+from .db import db
+
+class Task(db.Model):
+  __tablename__ = 'tasks'
+
+  id = db.Column(db.Integer, primary_key = True)
+  user_id = db.Column(db.Integer, db.ForeignKey("users.id"), nullable = False)
+  project_id = db.Column(db.Integer, db.ForeignKey("projects.id"), nullable = True)
+  name = db.Column(db.String(1000), nullable = False)
+  description = db.Column(db.String, nullable = True)
+  priority = db.Column(db.String(255), nullable = False)
+  due_date = db.Column(db.Date, nullable = True)
+  user = db.relationship("User", back_populates="tasks")
+  project = db.relationship("Project", back_populates="projects")
+
+  def to_dict(self):
+    return {
+      "id": self.id,
+      "user_id": self.user_id,
+      "project_id": self.project_id,
+      "name": self.name,
+      "description": self.description,
+      "priority": self.priority,
+      "due_date": self.due_date,
+    }
--- app/models/user.py
+++ app/models/user.py
@@ -9,6 +9,7 @@ class User(db.Model, UserMixin):
   username = db.Column(db.String(40), nullable = False, unique = True)
   email = db.Column(db.String(255), nullable = False, unique = True)
   hashed_password = db.Column(db.String(255), nullable = False)
+  profile_pic = db.Column(db.String(1000), nullable = True)
 
 
   @property
@@ -29,5 +30,6 @@ class User(db.Model, UserMixin):
     return {
       "id": self.id,
       "username": self.username,
-      "email": self.email
+      "email": self.email,
+      "profile_pic": self.profile_pic
     }
