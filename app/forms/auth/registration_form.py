aeb246d67577be3f741194ad4fdf324a4012528f
Daniel Upchurch <danielupchurch93@gmail.com>
2021-05-07 12:33:18 -0400

    registration form created

---
+++ app/forms/auth/registration_form.py
@@ -0,0 +1,21 @@
+from flask_wtf import FlaskForm  # noqa
+from wtforms import StringField  # noqa
+from wtforms.validators import DataRequired, Email, ValidationError  # noqa
+from app.models import User
+
+
+def user_exists(form, field):
+    email = field.data
+    user = User.query.filter(User.email == email).first()
+    if user:
+        raise ValidationError("User is already registered.")
+
+# check if the password contains at least one uppercase, one lower case, and one special character with length > 6
+def password_format(form, field):
+    password = field.data
+    
+
+class RegistrationForm(FlaskForm):
+    username = StringField("username", validators=[DataRequired()])
+    email = StringField("email", validators=[DataRequired(), user_exists])
+    password = StringField("password", validators=[DataRequired(), password_format])
