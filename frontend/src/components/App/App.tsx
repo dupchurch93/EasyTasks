965727813df98c706fab767bc666a22bded10848
Antonio <anguilar@aol.com>
2021-05-17 11:51:46 -0700

    working on splash

--- frontend/src/components/App/App.tsx
+++ frontend/src/components/App/App.tsx
@@ -5,6 +5,7 @@ import { authenticate } from "../../store/session";
 import LoginForm from "../Auth/LoginForm";
 import SignUpForm from "../Auth/SignUpForm";
 import "./App.css";
+import Splash from "../Sections/Splash";
 
 function App() {
   const dispatch = useDispatch();
@@ -19,10 +20,13 @@ function App() {
       {/* Navbar will be here */}
       <Switch>
         <Route path="/" exact={true}>
-          <LoginForm></LoginForm>
+          <Splash />
+        </Route>
+        <Route path="/login" exact={true}>
+          <LoginForm />
         </Route>
         <Route path="/sign-up" exact={true}>
-          <SignUpForm></SignUpForm>
+          <SignUpForm />
         </Route>
       </Switch>
     </BrowserRouter>
---
+++ frontend/src/components/Sections/Splash/Splash.tsx
@@ -0,0 +1,12 @@
+function Splash() {
+  return (
+    <div>
+      <h1>Wow</h1>
+      <p>Wow</p>
+      <input type="text" />
+      <button>Wow</button>
+    </div>
+  );
+}
+
+export default Splash;
