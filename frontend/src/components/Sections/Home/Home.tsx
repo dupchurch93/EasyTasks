6c7a1afc1bc1c39190d218c4109e57fe04b15959
Daniel Upchurch <danielupchurch93@gmail.com>
2021-05-20 13:02:05 -0400

    added basic layout for daily home page

--- frontend/src/components/Sections/Home/Home.tsx
+++ frontend/src/components/Sections/Home/Home.tsx
@@ -1,7 +1,9 @@
 const Home = () => {
     return (
-        <div>
-            This is our home component. Yay
+        <div className="grid grid-rows-standardLayout">
+            <div className="flex justify-center items-center bg-royalBlue h-24">Header</div>
+            <div className="">Content</div>
+            <div className="flex justify-center items-center text-white bg-black h-24">Footer</div>
         </div>
     )
 }
