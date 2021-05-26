09074d2ad9cf242a5b024497e7daac219abae13c
Daniel Upchurch <danielupchurch93@gmail.com>
2021-05-26 12:36:44 -0400

    changed buttons to Links to allow navigating from splash page

--- frontend/src/components/Sections/Splash/Splash.tsx
+++ frontend/src/components/Sections/Splash/Splash.tsx
@@ -1,5 +1,6 @@
-import greenSticky from "../../../images/greenSticky.svg"
-import yellowSticky from "../../../images/yellowSticky.svg"
+import { Link } from "react-router-dom";
+import greenSticky from "../../../images/greenSticky.svg";
+import yellowSticky from "../../../images/yellowSticky.svg";
 
 function Splash() {
   return (
@@ -7,16 +8,41 @@ function Splash() {
       <div className="self-center ml-twenty  h-72 w-72">
         <h1 className="text-7xl mb-4">EasyTask</h1>
         <h2 className="text-2xl mb-8">Planning made simple</h2>
-        <button className="bg-royalBlue hover:bg-royalBlueLight text-white font-bold py-2 px-6 rounded-full m-1">
-          TRY NOW</button>
+        <Link to="/sign-up">
           <button className="bg-royalBlue hover:bg-royalBlueLight text-white font-bold py-2 px-6 rounded-full m-1">
-          LOG IN</button>
+            TRY NOW
+          </button>
+        </Link>
+        <Link to="/login">
+          <button className="bg-royalBlue hover:bg-royalBlueLight text-white font-bold py-2 px-6 rounded-full m-1">
+            LOG IN
+          </button>
+        </Link>
       </div>
-      <img className="absolute top-28 right-56 h-64 w-64" src={greenSticky} alt="Green sticky note"></img>
-      <img className="absolute top-56 right-1/4 h-64 w-64 z-10" src={yellowSticky} alt="Yellow sticky note"></img>
+      <img
+        className="absolute top-28 right-56 h-64 w-64"
+        src={greenSticky}
+        alt="Green sticky note"
+      ></img>
+      <img
+        className="absolute top-56 right-1/4 h-64 w-64 z-10"
+        src={yellowSticky}
+        alt="Yellow sticky note"
+      ></img>
       <div className="h-screen bg-blue-600">
-        <svg xmlns="http://www.w3.org/2000/svg" className="h-8 w-8 ml-auto m-4" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-          <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16"/>
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          className="h-8 w-8 ml-auto m-4"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke="currentColor"
+        >
+          <path
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            stroke-width="2"
+            d="M4 6h16M4 12h16M4 18h16"
+          />
         </svg>
       </div>
     </div>
