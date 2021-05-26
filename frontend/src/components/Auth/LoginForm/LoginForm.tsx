d12d56b41f56ecfdcabab55d0f6dacf5745f16e9
Lane <lanedaltonjohns@gmail.com>
2021-05-26 13:02:03 -0400

    add background to page

--- frontend/src/components/Auth/LoginForm/LoginForm.tsx
+++ frontend/src/components/Auth/LoginForm/LoginForm.tsx
@@ -31,23 +31,27 @@ const LoginForm = () => {
   }
 
   return (
-    <div>
+    <div  className=""    style={{
+              backgroundImage: `url(
+              "https://static.vecteezy.com/system/resources/thumbnails/001/834/369/small/collection-of-different-colored-sheets-of-sticky-notes-vector.jpg"
+              )`,
+            }}>
       <form data-testid="login-form" onSubmit={onLogin}>
         <div>
           {/* {errors.map((error) => (
           <div>{error}</div>
         ))} */}
         </div>
-        <div className="flex max-w-sm mx-auto my-24 overflow-hidden bg-white rounded-lg shadow-lg dark:bg-gray-800 lg:max-w-4xl">
-          <div
+        <div className="flex max-w-sm mx-auto my-24 overflow-hidden bg-white rounded-lg shadow-lg dark:bg-gray-800 lg:max-w-xl">
+          {/* <div
             className="hidden bg-cover lg:block lg:w-1/2"
             style={{
               backgroundImage: `url(
               "https://images.unsplash.com/photo-1606660265514-358ebbadc80d?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1575&q=80"
               )`,
             }}
-          ></div>
-          <div className="w-full px-6 py-8 md:px-8 lg:w-1/2">
+          ></div> */}
+          <div className="w-full px-6 py-8 md:px-8 ">
             <h2 className="text-2xl font-semibold text-center text-gray-700 dark:text-white">
               EasyTask
             </h2>
